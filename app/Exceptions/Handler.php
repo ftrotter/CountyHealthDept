273c1fbfcefb3496badb66b7c0659dd32ba2334a
Fred Trotter <fred.trotter@gmail.com>
2019-12-08 07:14:14 +0000

    enable ignition error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,7 +7,19 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
-    /**
+
+	// added per https://flareapp.io/docs/ignition-for-laravel/installation
+protected function whoopsHandler()
+{
+    try {
+        return app(\Whoops\Handler\HandlerInterface::class);
+    } catch (\Illuminate\Contracts\Container\BindingResolutionException $e) {
+        return parent::whoopsHandler();
+    }
+}	
+
+
+	/**
      * A list of the exception types that are not reported.
      *
      * @var array
