e7c6ee7711db4c27b3a4da4012ca972a3dbd26af
Fred Trotter <fred.trotter@gmail.com>
2019-12-29 23:03:12 -0600

    routes for the two linkers

--- routes/starting.web.php
+++ routes/starting.web.php
@@ -19,12 +19,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//The Generic linker works to make a lightweight tripestore out of DURC objects. Specifically object <-- tag --> object
-//this will show the 'add new triples' form, or if the triples table is not yet built, show the SQL to build it.
-Route::get('genericLinkerForm/{durc_type_left}/{durc_type_right}/{durc_type_link}','GenericLinker@linkForm');
+//First we do the "triples" which give the links types.. https://en.wikipedia.org/wiki/Triplestore
+Route::get('genericLinkerForm/{durc_type_left}/{durc_type_right}/{durc_type_link}','GenericTripleLinker@linkForm');
 // this saves the triples form POSTs
-Route::post('genericLinkerSave/{durc_type_left}/{durc_type_right}/{durc_type_link}','GenericLinker@linkSaver');
+Route::post('genericLinkerSave/{durc_type_left}/{durc_type_right}/{durc_type_link}','GenericTripleLinker@linkSaver');
 // this will always show the SQL to build to triples table, as well as the SQL to generate a Zermelo Graph report from the triple... even if the database already exists..
-Route::get('genericLinkerSQL/{durc_type_left}/{durc_type_right}/{durc_type_link}','GenericLinker@showSQLView');
+Route::get('genericLinkerSQL/{durc_type_left}/{durc_type_right}/{durc_type_link}','GenericTripleLinker@showSQLView');
+
+
+//First we do the "triples" which give the links types.. https://en.wikipedia.org/wiki/Triplestore
+Route::get('genericLinkerForm/{durc_type_left}/{durc_type_right}','GenericLinker@linkForm');
+// this saves the triples form POSTs
+Route::post('genericLinkerSave/{durc_type_left}/{durc_type_right}','GenericLinker@linkSaver');
+// this will always show the SQL to build to triples table, as well as the SQL to generate a Zermelo Graph report from the triple... even if the database already exists..
+Route::get('genericLinkerSQL/{durc_type_left}/{durc_type_right}','GenericLinker@showSQLView');
+
+
 
 
